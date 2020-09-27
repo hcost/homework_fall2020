56110d54f07d046258730ee90538d0dcc895aeb9
Harrison Costantino <harrisoncostantino@berkeley.edu>
2020-09-27 12:53:51 -0700

    finish hw1

--- hw1/cs285/policies/MLP_policy.py
+++ hw1/cs285/policies/MLP_policy.py
@@ -81,7 +81,8 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
             observation = obs[None]
 
         # TODO return the action that the policy prescribes
-        raise NotImplementedError
+        action = self(obs)
+        return ptu.to_numpy(action)
 
     # update/train this policy
     def update(self, observations, actions, **kwargs):
@@ -93,7 +94,9 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
     # return more flexible objects, such as a
     # `torch.distributions.Distribution` object. It's up to you!
     def forward(self, observation: torch.FloatTensor) -> Any:
-        raise NotImplementedError
+        mean = self.mean_net(ptu.from_numpy(observation))
+        dist = torch.distributions.Normal(mean, torch.exp(self.logstd))
+        return dist.rsample()
 
 
 #####################################################
@@ -109,7 +112,13 @@ class MLPPolicySL(MLPPolicy):
             adv_n=None, acs_labels_na=None, qvals=None
     ):
         # TODO: update the policy and return the loss
-        loss = TODO
+        self.optimizer.zero_grad()
+        sampled = self(observations)
+        actions = ptu.from_numpy(actions)
+        loss = self.loss(actions, sampled)
+        loss.backward()
+        self.optimizer.step()
+
         return {
             # You can add extra logging information here, but keep this line
             'Training Loss': ptu.to_numpy(loss),
