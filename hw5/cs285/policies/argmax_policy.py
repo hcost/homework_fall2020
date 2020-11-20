b46806733e8b01cd0f3355ba13789daa30e2b15d
Harrison Costantino <harrisoncostantino@berkeley.edu>
2020-11-20 13:35:17 -0800

    added old solns

--- hw5/cs285/policies/argmax_policy.py
+++ hw5/cs285/policies/argmax_policy.py
@@ -7,18 +7,12 @@ class ArgMaxPolicy(object):
     def __init__(self, critic):
         self.critic = critic
 
-    def set_critic(self, critic):
-        self.critic = critic
-
     def get_action(self, obs):
-        # MJ: changed the dimension check to a 3
         if len(obs.shape) > 3:
             observation = obs
         else:
             observation = obs[None]
 
-        raise NotImplementedError
-        # TODO: get this from hw3
-
-    ####################################
-    ####################################
+        vals = self.critic.qa_values(observation)
+        action = np.argmax(vals)
+        return action.squeeze()
